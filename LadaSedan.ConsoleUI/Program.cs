ecbc43a5461fc41b40ac33bb5929379b6d9c482f
taihon <taihon@users.noreply.github.com>
2017-05-06 10:02:51 +0700

    Add project files.

---
+++ LadaSedan.ConsoleUI/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LadaSedan.ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
